a1191050b45b60168a004a918b33f1e3ba652a58
thandayuthapani <thandayuthapani.s@huawei.com>
2018-02-23 17:38:19 +0530

    output file directory change

--- scripts/get_output.sh
+++ scripts/get_output.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-sudo docker cp scripts_dubboconsumer_1:/app/logs/stdout.log ./output/dubbo
+sudo docker cp scripts_dubboconsumer_1:/app/logs/stdout.log dubbo_stdout.log
 
-sudo docker cp scripts_cseconsumer_1:/app/logs/stdout.log ./output/cse
+sudo docker cp scripts_cseconsumer_1:/app/logs/stdout.log cse_stdout.log
